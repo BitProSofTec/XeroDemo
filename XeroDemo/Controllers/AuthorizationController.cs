590413af21bc92c22973c08f0e3234425b713114
MyCodeClub <ankitsahay1990@gmail.com>
2020-07-18 18:49:51 +0530

    Added Authenticator and XeroApiHelper

---
+++ XeroDemo/Controllers/AuthorizationController.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc;
+using Xero.Api.Example.Applications.Public;
+using Xero.Api.Example.MVC.Helpers;
+using Xero.Api.Infrastructure.OAuth;
+
+
+namespace XeroDemo.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private string clientId = "";
+        private string clientSecret = "";
+
+        public ActionResult Index()
+        {
+            var xeroAuthorizeUri = new RequestUrl("https://login.xero.com/identity/connect/authorize");
+            var url = xeroAuthorizeUri.CreateAuthorizeUrl(
+             clientId: clientId,
+             responseType: "code", //hardcoded authorisation code for now.
+             redirectUri: "https://developer.xero.com",
+             state: "your state",
+             scope: "openid profile email files accounting.transactions accounting.transactions.read accounting.reports.read accounting.journals.read accounting.settings accounting.settings.read accounting.contacts accounting.contacts.read accounting.attachments accounting.attachments.read offline_access"
+         );
+            return View();
+        }
+    }
+}
---
+++ XeroDemo/Helper/XeroApiHelper.cs
@@ -0,0 +1,68 @@
+﻿//using System;
+//using Xero.Api.Core;
+//using Xero.Api.Example.Applications.Partner;
+//using Xero.Api.Example.Applications.Public;
+//using Xero.Api.Infrastructure.Interfaces;
+//using Xero.Api.Infrastructure.OAuth;
+//using Xero.Api.Example.TokenStores;
+//using Xero.Api.Serialization;
+//using System.Configuration;
+//namespace XeroDemo.Helpers
+//{
+//    public class ApplicationSettings
+//    {
+//        public string BaseApiUrl { get; set; }
+//        public Consumer Consumer { get; set; }
+//        public object Authenticator { get; set; }
+
+//    }
+//    public static class XeroApiHelper
+//    {
+//        private static ApplicationSettings _applicationSettings;
+//        static XeroApiHelper()
+//        {
+
+//            var callbackUrl = ConfigurationManager.AppSettings["callbackUrl"];
+//            var memoryStore = new MemoryTokenStore();
+//            var requestTokenStore = new MemoryTokenStore();
+//            var baseApiUrl = ConfigurationManager.AppSettings["baseApiUrl"];
+
+//            // Consumer details for Application
+//            var consumerKey = ConfigurationManager.AppSettings["consumerKey"];
+//            var consumerSecret = ConfigurationManager.AppSettings["consumerSecret"];
+//            // Public Application Settings
+//            var publicConsumer = new Consumer(consumerKey, consumerSecret);
+//            var publicAuthenticator = new PublicMvcAuthenticator(baseApiUrl, baseApiUrl, callbackUrl, memoryStore,
+//                publicConsumer, requestTokenStore);
+//            var publicApplicationSettings = new ApplicationSettings
+//            {
+//                BaseApiUrl = baseApiUrl,
+//                Consumer = publicConsumer,
+//                Authenticator = publicAuthenticator
+//            };
+//            _applicationSettings = publicApplicationSettings;
+//        }
+//        public static ApiUser User()
+//        {
+//            return new ApiUser { Name = Environment.MachineName };
+//        }
+//        public static IConsumer Consumer()
+//        {
+//            return _applicationSettings.Consumer;
+//        }
+//        public static IMvcAuthenticator MvcAuthenticator()
+//        {
+//            return (IMvcAuthenticator)_applicationSettings.Authenticator;
+//        }
+//        public static IXeroCoreApi CoreApi()
+//        {
+//            if (_applicationSettings.Authenticator is IAuthenticator)
+//            {
+//                return new XeroCoreApi(_applicationSettings.BaseApiUrl, _applicationSettings.Authenticator as IAuthenticator,
+//                    _applicationSettings.Consumer, User(), new DefaultMapper(), new DefaultMapper());
+//            }
+
+//            return null;
+//        }
+//    }
+//}
